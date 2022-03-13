68e9b3801a907da5df88f9339a201a7f96938177
LovedeepSaini <saini.lovedeep.kaur@gmail.com>
2022-03-13 13:13:22 +0000

    Add files via upload

---
+++ project1/Gazetteer/libs/js/app.js
@@ -0,0 +1,116 @@
+
+  $('#btnRun').click(function() {
+
+  
+  $.ajax({
+    url: "libs/php/getCountryInfo.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      country:$('#country-dropdown').val(),
+      lang: $('#selLanguage').val(),
+    },
+    success: function(result) {
+     
+     
+      console.log(JSON.stringify(result));
+       
+      
+
+      if (result.status.name == "ok") {
+
+       
+        $('#txtCapital').html(result.data[0].capital);
+        $('#txtLanguages').html(result.data[0].languages);
+        $('#txtPopulation').html(result.data[0].population);
+        $('#txtArea').html(result.data[0].areaInSqKm);
+        $('#txtSouth').html(result.data[0].south);
+       $('#txtNorth').html(result.data[0].north);
+        $('#txtEast').html(result.data[0].east);
+        $('#txtWest').html(result.data[0].west);
+        $('#txtcurrencycode').html(result.data[0].currencyCode);
+
+
+      }
+      
+      
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // your error code
+    }
+  });
+})
+
+
+
+
+
+
+//news
+$('#btnRun4').click(function() {
+  
+  $.ajax({
+    url: "libs/php/getNews.php",
+    type: 'GET',
+    dataType: 'json',
+    data: {
+        newsCountry: $('#country-dropdown').val(),
+    },
+    success: function(result) {
+        console.log('News Data', result);
+        if (result.status == "No matches for your search.") {
+            $('#Headlines').hide();
+            $('#newsList').hide();
+            $('#noNews').html('Sorry, the Newscatcher API does not have articles for this country.');
+        }
+        else if (result.status == "ok") {
+            $('#newsList').html("");
+            for (var i=0; i<result.articles.length; i++) {
+                $("#newsList").append('<li><a href='+ result.articles[i].link + '>' + result.articles[i].title + '</a></li>');
+        }                
+    }},
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+    }
+  }); 
+  });
+  $('#btnRun2').click(function() {
+
+    $.ajax({
+      url: "libs/php/wikipedia.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        q: $('#country-dropdown').val(),
+        maxRows: 10,
+       
+        
+      },
+      success: function(result) {
+
+        console.log(JSON.stringify(result));
+
+        if (result.status.name == "ok") {
+
+          $('#txtlatitude2').html(result['data'][0]['lat']);
+          $('#txtlongitude2').html(result['data'][0]['lng']);
+          $('#txtwikipediaurl').html(result['data'][0]['wikipediaUrl']);
+          $('#txtelevation').html(result['data'][0]['elevation']);
+          $('#txtlang').html(result['data'][0]['lang']);
+          $('#txttitle').html(result['data'][0]['title']);
+          $('#txtfeature').html(result['data'][0]['feature']);
+          $('#txtrank').html(result['data'][0]['rank']);
+          $('#txtsummary').html(result['data'][0]['summary']);
+          $('#txtcountrycode').html(result['data'][0]['countryCode']);
+          
+          
+        }
+      
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log("data not available ")
+      }
+    }); 
+  
+  });
+   
